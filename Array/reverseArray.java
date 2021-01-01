c89c00017b8c4425e7a05599fe03753c4ddf1272
ZymorX-pt <prathamesh.oscp@gmail.com>
2021-01-01 15:36:32 +0530

    init

---
+++ Array/reverseArray.java
@@ -0,0 +1,43 @@
+// Program to reverse an array
+
+package Array;
+
+public class reverseArray {
+
+    static void reverseArr(int[] arr, int start, int end) {
+
+        int temp;
+
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void printArray(int[] arr, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6};
+        System.out.print("Entered string is: ");
+        printArray(arr, 6);
+        reverseArr(arr, 0, 5);
+        System.out.print("Reversed string is: ");
+        printArray(arr, 6);
+    }
+}
+
+/* 
+Output: 
+
+Entered string is: 1 2 3 4 5 6
+Reversed string is: 6 5 4 3 2 1
+*/
