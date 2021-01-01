6c69954d31a54c61def754b33b61a57dfc83b222
ZymorX-pt <prathamesh.oscp@gmail.com>
2021-01-01 16:05:42 +0530

    final code with user input

--- Array/reverseArray.py
+++ Array/reverseArray.py
@@ -17,3 +17,19 @@ print("Entered array is ",end="")
 print(arr)
 print("Reversed array is ",end="")
 reverseArray(arr)
+
+
+"""
+Output:
+
+Enter length of your array: 6
+Enter your array:
+123
+789
+abc
+xyz
+???
+1b4
+Entered array is ['123', '789', 'abc', 'xyz', '???', '1b4']
+Reversed array is ['1b4', '???', 'xyz', 'abc', '789', '123']
+"""
